bf60de6ad41aecb64277babd90d53615b5691500
Alexis Perez <45041860+Alecxz20@users.noreply.github.com>
2023-07-15 15:06:51 -0500

    Updates

---
+++ precache-manifest.595ce42587426c7a5727bb926bf67cc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02476db59c80460b976b",
+    "url": "/todo/static/css/main.5396d326.chunk.css"
+  },
+  {
+    "revision": "02476db59c80460b976b",
+    "url": "/todo/static/js/main.02476db5.chunk.js"
+  },
+  {
+    "revision": "893881008128fd043f36",
+    "url": "/todo/static/js/1.89388100.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "07ac4f3432ba6e53933103466b1fc351",
+    "url": "/todo/index.html"
+  }
+];
